ddb017546d0db159748043006f829b791e817627
anusha-anil3773 <108619912+anusha-anil3773@users.noreply.github.com>
2022-11-16 16:43:49 +0530

    herolu commit

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,82 @@
 
 
-const path = require('path');
+
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan') // for seeing api calls in terminal
-const PORT = 3000
+
 const app = new express()
 
 
 // require('dotenv').config()
 
-require('./middlewares/mongoDB') //to init mongoDB
-const DATA = require('../models/blog')
+// require('./middlewares/mongoDB') //to init mongoDB
+// const DATA = require('../models/blog')
 
 app.use(cors()) //to connect frontend and backend without any disturbance
 app.use(express.json()) // to recieve data from front end
 app.use(express.urlencoded({ extended: true }))
 app.use(logger('dev'))
-app.use(express.static('./dist/frontend'));
 
 
+
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const blogSchema = new Schema({
+    placename: {
+        type: String,
+        required: true
+    },
+    follow: {
+        type: Number,
+        required: true
+    },
+    articlecontent: {
+        type: String,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    date:{
+        type:Date,
+        default:Date.now()
+    }
+})
+
+
+let blogDATA = mongoose.model('article', blogSchema)
+
+
+mongoose.connect("mongodb+srv://Anusha37:anusha123@cluster0.bd302zx.mongodb.net/Article?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}). then(()=>{
+    console.log('mongoDB connected successfully!!!')
+}).catch((error)=>{
+    console.log(error.message)
+})
+
+const router = express.Router() //routing function
 
 
 // students full list read 
-router.get('/api/bloglist', async (req, res) => {
+// router.get('/api/bloglist', async (req, res) => {
 
-    try {
+//     try {
 
-        const list = await DATA.find()
-        res.send(list)
+//         const list = await DATA.find()
+//         res.send(list)
 
 
-    } catch (error) {
-        console.log(error)
-    }
+//     } catch (error) {
+//         console.log(error)
+//     }
 
-})
+// })
 
 
 //  add 
@@ -68,18 +109,18 @@ router.post('/api/blog', async (req, res) => {
 
 // delete 
 
-router.delete('/api/blog/:id', async (req, res) => {
-    try {
-        let id = req.params.id
-        const deleteblog = await DATA.findByIdAndDelete(id)
-        res.send(deleteblog)
+// router.delete('/api/blog/:id', async (req, res) => {
+//     try {
+//         let id = req.params.id
+//         const deleteblog = await DATA.findByIdAndDelete(id)
+//         res.send(deleteblog)
 
 
-    } catch (error) {
-        console.log(error)
+//     } catch (error) {
+//         console.log(error)
 
-    }
-})
+//     }
+// })
 
 
 // update 
@@ -109,18 +150,18 @@ router.put('/api/blog', async (req, res) => {
 // Single  detail 
 
 
-router.get('/api/blog/:id', async (req, res) => {
-    try {
+// router.get('/api/blog/:id', async (req, res) => {
+//     try {
 
-        let id = req.params.id
-        const singleblog = await DATA.findById(id)
-        res.send(singleblog)
+//         let id = req.params.id
+//         const singleblog = await DATA.findById(id)
+//         res.send(singleblog)
 
-    } catch (error) {
-        console.log(error)
+//     } catch (error) {
+//         console.log(error)
 
-    }
-})
+//     }
+// })
 
 
 
@@ -131,11 +172,15 @@ router.get('/api/blog/:id', async (req, res) => {
 // const api = require('./routes/api')
 // app.use('/api',api)
 
-app.get('/*', function(req,res)
 
-{res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));});
-
+var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`...........Server running at ${PORT}.............`)
 })
+
+const path = require('path');
+app.use(express.static(path.join(__dirname+'/dist/frontend/index.html')));
+app.get('/*', function(req,res)
+
+{res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));});
--- backend/models/blog.js
+++ backend/models/blog.js
@@ -1,32 +1,32 @@
 
 
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+// const mongoose = require('mongoose')
+// const Schema = mongoose.Schema
 
-const blogSchema = new Schema({
-    placename: {
-        type: String,
-        required: true
-    },
-    follow: {
-        type: Number,
-        required: true
-    },
-    articlecontent: {
-        type: String,
-        required: true
-    },
-    comment: {
-        type: String,
-        required: true
-    },
-    date:{
-        type:Date,
-        default:Date.now()
-    }
-})
+// const blogSchema = new Schema({
+//     placename: {
+//         type: String,
+//         required: true
+//     },
+//     follow: {
+//         type: Number,
+//         required: true
+//     },
+//     articlecontent: {
+//         type: String,
+//         required: true
+//     },
+//     comment: {
+//         type: String,
+//         required: true
+//     },
+//     date:{
+//         type:Date,
+//         default:Date.now()
+//     }
+// })
 
 
-let blogDATA = mongoose.model('article', blogSchema)
+// let blogDATA = mongoose.model('article', blogSchema)
 
-module.exports = blogDATA
+// module.exports = blogDATA
